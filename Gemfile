54e8c9d7a831ecfa3a319c8a027c51c3114cf593
yyh-gl <yhonda.95.gl@gmail.com>
2018-09-06 11:42:01 +0900

    LINE通知機能追加

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'google-api-client'
 gem 'dotenv-rails'
+gem 'activesupport'
---
+++ line.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+require 'active_support/time'
+require_relative 'calendar.rb'
+
+messages = [{type: 'text', text: '↓本日の予定↓'}]
+
+cal = Calendar.new
+events = cal.fetch_calender_events
+events.each do |e|
+  if e.start.date.present?
+    start = Date.parse(e.start.date)
+    str_start = e.start.date + ' 一日中'
+  else
+    start = Date.parse(e.start.date_time.strftime('%Y-%m-%d'))
+    str_start = e.start.date_time.strftime('%Y-%m-%d %H:%M:%S 〜')
+  end
+  from = Date.today
+  untill = Date.today + 1.day
+  
+  if from <= start && start < untill
+    puts "- #{e.summary} (#{str_start})"
+    messages << {type: 'text', text: "#{e.summary} (#{str_start})"}
+  end
+end
+
+messages << {type: 'text', text: '↑以上↑'}
+
+cmd = <<"EOS"
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {#{ENV['ACCESS_TOKEN']}}' \
+-d '{
+    "to": "#{ENV['CHANNEL_ID']}",
+    "messages": #{messages.to_json}
+}' https://api.line.me/v2/bot/message/push
+EOS
+
+exec(cmd)
